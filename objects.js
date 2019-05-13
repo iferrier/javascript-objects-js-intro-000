20f4ca9aecf0e52da04ccf903303d36b68f28ead
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-05-13 12:08:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
-  return(playlist);
 }
 
 var playlist = new Object();
